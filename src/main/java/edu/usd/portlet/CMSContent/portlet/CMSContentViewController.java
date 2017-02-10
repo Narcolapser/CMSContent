107ccf4b0e7e3c52f3d7e757b21878f844144ceb
Toben Archer <toben.archer@usd.edu>
2017-02-10 16:26:39 -0600

    It's starting to get off the ground.
    I'm not sure what I am missing to get it to actually display something at
    this point. I'll have to keep sloughing through and comparing it to other
    portlets.

---
+++ src/main/java/edu/usd/portlet/CMSContent/portlet/CMSContentViewController.java
@@ -0,0 +1,61 @@
+/**
+ * Licensed to Apereo under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Apereo licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License.  You may obtain a
+ * copy of the License at the following location:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package edu.usd.portlet.cmscontent.portlet;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.portlet.PortletPreferences;
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.ResourceMapping;
+import org.springframework.web.portlet.ModelAndView;
+
+/**
+ * This class handles retrieves information and displays the view page.
+ * 
+ * @author Toben Archer
+ * @version $Id$
+ */
+@Controller
+@RequestMapping("VIEW")
+public class CMSContentViewController {
+    protected final Log logger = LogFactory.getLog(this.getClass());
+
+    @RequestMapping
+    public ModelAndView viewWeather(RenderRequest request, RenderResponse response) {
+        String viewName = "view";
+        
+        //show view.jsp with a model named 'weather' populated weather data
+        final Map<String, Object> model = new LinkedHashMap<String, Object>();
+        model.put("Test","like a Bosch.");
+        return new ModelAndView(viewName,model);
+    }
+}
