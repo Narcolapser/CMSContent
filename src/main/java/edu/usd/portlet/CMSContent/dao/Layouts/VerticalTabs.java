b629600cd04a7c833f4d9b6dee3bb13c6527ee31
Toben Archer <Toben.Archer@usd.edu>
2017-08-02 15:54:12 -0500

    Most of the interface is done.
    I made some slow progress today. But I've got most of the interface
    fleshed out. Now I just need to wire up the functionality. I've got
    the portlet view type updating when you change the view type to
    show you the different vew descriptions, and that's about it.
    
    Left to do: Document source changes list of documents.
    List of documents is a chosen list.
    Up and Down buttons for page positions.
    remove button for pages.
    
    There is the whole matter of properties, but that's down the road
    a bit yet. Currently none of the views actually make use of
    properties. Vertical tabs will, allowing you to set a column width
    for the tab column, but it currently does not.

--- src/main/java/edu/usd/portlet/CMSContent/dao/Layouts/VerticalTabs.java
+++ src/main/java/edu/usd/portlet/CMSContent/dao/Layouts/VerticalTabs.java
@@ -22,6 +22,10 @@ public class VerticalTabs extends CMSLayout
 	public VerticalTabs(){}
 	public String getName(){return "Vertical Tabs";}
 	public String getView(){return "view_vertical_tabs";}
+	public String getDescription()
+	{
+		return "Tabs, but vertically along the left side. Also included on the tabs is a button to copy a direct link to that portlet fullscreened with that tab open. \n\nThere is one property: Tab Width. This specifies how wide the column of tabs should be. Useful for documents with really long titles.";
+	}
 
 	public CMSLayout copy(CMSLayout val)
 	{
