d6759d740fcfb425b50bb414d912bac0699d2a1f
Toben Archer <Toben.Archer@usd.edu>
2018-01-18 16:56:06 -0600

    Content retrieval stream lined.
    Sort of at least. I moved the retrieval of content from the
    controller into the CMSLayout class. This is done in preparation
    for merging with the search branch. The code that was used for
    retreiving was reproduced in both of the controllers. So moving it
    to the central place of the CMSLayout class makes the code a little
    more DRY.
    
    Last thing to do before merging is to comment that process. It is a
    long convoluted process that is not easy to understand. It would be
    nice if I could also break it down into some methods. Right now it
    has 3 nested for loops at it's worst.

--- src/main/java/edu/usd/portlet/CMSContent/dao/CMSLayout.java
+++ src/main/java/edu/usd/portlet/CMSContent/dao/CMSLayout.java
@@ -5,8 +5,20 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
 
+import javax.portlet.RenderRequest;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.ModelAndView;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONArray;
 
 import edu.usd.portlet.cmscontent.dao.CMSSubscription;
 import org.springframework.web.portlet.ModelAndView;
@@ -152,5 +164,68 @@ public class CMSLayout
 	{
 		logger.info("Layout Copying is copying");
 		return new CMSLayout(val);
+	}
+	
+	public ArrayList<CMSDocument> getContent(RenderRequest request, List<CMSDocumentDao> dataSources)
+	{
+	//Preparing a the list of page content.
+		ArrayList<CMSDocument> content = new ArrayList<CMSDocument>();
+		Map<String, Boolean> isForm = new HashMap<String, Boolean>();
+		Map<String, ArrayList<JSONObject>> formContent = new HashMap<String, ArrayList<JSONObject>>();
+		JSONArray obj;
+
+		for(CMSSubscription sub:this.getSubscriptions())
+			for(CMSDocumentDao ds:dataSources)
+				if(ds.getDaoName().equals(sub.getDocSource()))
+				{
+					List<String> groups = sub.getSecurityGroups();
+					if (groups == null || groups.size() == 0 || groups.get(0).equals(""))
+					{
+						content.add(ds.getDocument(sub.getDocId()));
+						if(sub.getDocId().contains("form:"))
+						{
+							isForm.put(sub.getDocId(),true);
+							try{
+								obj = new JSONArray(ds.getDocument(sub.getDocId()).getContent());
+								ArrayList<JSONObject> jobj = new ArrayList<JSONObject>();
+								for(int i = 0; i < obj.length(); i++)
+									jobj.add(obj.getJSONObject(i));
+								formContent.put(sub.getDocId(),jobj);
+							}
+							catch(JSONException e)
+							{
+								logger.error("Error loading form data: " + e);
+							}
+						}
+						else
+							isForm.put(sub.getDocId(),false);
+					}
+					else
+					{
+						for(String role : sub.getSecurityGroups())
+							if(request.isUserInRole(role))
+							{
+								content.add(ds.getDocument(sub.getDocId()));
+								if(sub.getDocId().contains("form:"))
+								{
+									isForm.put(sub.getDocId(),true);
+									try{
+										obj = new JSONArray(ds.getDocument(sub.getDocId()).getContent());
+										ArrayList<JSONObject> jobj = new ArrayList<JSONObject>();
+										for(int i = 0; i < obj.length(); i++)
+											jobj.add(obj.getJSONObject(i));
+										formContent.put(sub.getDocId(),jobj);
+									}
+									catch(JSONException e)
+									{
+										logger.error("Error loading form data: " + e);
+									}
+								}
+								else
+									isForm.put(sub.getDocId(),false);
+							}
+					}
+				}
+		return content;
 	}
 }
