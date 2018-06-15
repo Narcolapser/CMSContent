3afbe3b67f5bf482820677c8669b43482426697a
Toben Archer <Toben.Archer@usd.edu>
2018-06-15 13:31:26 -0500

    Missed commit.
    Should have done this before I left for the conference...

--- src/main/java/edu/usd/portlet/CMSContent/service/CMSSearchIndex.java
+++ src/main/java/edu/usd/portlet/CMSContent/service/CMSSearchIndex.java
@@ -5,40 +5,23 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
+import java.util.HashMap;
+//import java.util.Integer;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.BooleanQuery.Builder;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.RegexpQuery;
-import org.apache.lucene.search.BooleanClause;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.analysis.CharArraySet;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.RAMDirectory;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Component;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import edu.usd.portlet.cmscontent.dao.CMSDocument;
 import edu.usd.portlet.cmscontent.dao.CMSDocumentDao;
@@ -47,7 +30,6 @@ import edu.usd.portlet.cmscontent.dao.CMSDocumentDao;
 @Service
 public class CMSSearchIndex
 {
-
 	public static final String[] ENGLISH_STOP_WORDS = {
 	"a", "an", "and", "are", "as", "at", "be", "but", "by",
 	"for", "if", "in", "into", "is", "it",
@@ -56,10 +38,8 @@ public class CMSSearchIndex
 	"they", "this", "to", "was", "will", "with"
 	};
 	protected final Log logger = LogFactory.getLog(this.getClass());
-	private StandardAnalyzer analyzer;
-	private Directory index;
-	private IndexWriterConfig config;
-	private IndexWriter writer;
+	
+	Map<String,IndexEntry> index = new HashMap<>();
 	
 	@Autowired
 	List<CMSDocumentDao> dataSources;
@@ -69,23 +49,22 @@ public class CMSSearchIndex
 	{
 		try
 		{
-		//CharArraySet((Collection)Arrays.asList(this.ENGLISH_STOP_WORDS),true));
-			//CharArraySet stopers = new CharArraySet();
-			analyzer = new StandardAnalyzer(new CharArraySet(Arrays.asList(this.ENGLISH_STOP_WORDS),true));
-			index = new RAMDirectory();
-			config = new IndexWriterConfig(analyzer);
-			writer = new IndexWriter(index,config);
+
 			for(CMSDocumentDao dao: dataSources)
 			{
 				logger.debug("Indexing " + dao.getDaoName());
 				List<CMSDocument> docs = dao.getAllDocumentsContentless();
+				logger.debug("1");
 				for(CMSDocument doc: docs)
 				{
-					addDoc(dao.getDocument(doc.getId()));
+					logger.debug("2");
+					CMSDocument val = dao.getDocument(doc.getId());
+					IndexEntry ie = new IndexEntry(val);
+					logger.debug(ie);
+					index.put(dao.getDaoName() + doc.getId(),ie);
+					logger.debug(index.get(dao.getDaoName() + doc.getId()));
 				}
 			}
-			writer.commit();
-			writer.close();
 			logger.debug("Indexer built");
 		}
 		catch(Exception e)
@@ -94,60 +73,33 @@ public class CMSSearchIndex
 		}
 	}
 
-	public List<CMSDocument> search(String query)
+	public int search(String query, String doc_source, String doc_id)
 	{
-		List<CMSDocument> ret = new ArrayList<CMSDocument>();
+		int ret = 0;
 		try
 		{
-			//Query q = new QueryParser("content",analyzer).parse(query);
-			//BooleanQuery q = new BooleanQuery();
-			Builder bq = new Builder();
 			for(String term:query.split(" "))
 			{
-				if(term.length() == 1)
-					continue;				
-				bq.add(new TermQuery(new Term("content",""+term+"")),BooleanClause.Occur.SHOULD);
-				//bq.add(new RegexpQuery(new Term("content","[^a-zA-Z\\d\\s:]"+term+"")));
-			}
-			BooleanQuery q = bq.build();
-			logger.debug("Query: ");
-			logger.debug(q);
-			int hitsPerPage = 1;
-			IndexReader reader = DirectoryReader.open(index);
-			IndexSearcher searcher = new IndexSearcher(reader);
-			TopDocs docs = searcher.search(q,hitsPerPage);
-			ScoreDoc[] hits = docs.scoreDocs;
-			if(hits.length > 0)
-			{
-//				logger.debug("Found " + hits.length + " hits.");
-				for(ScoreDoc hit: hits)
+				if(Arrays.asList(ENGLISH_STOP_WORDS).contains(term))
+					continue;
+				IndexEntry val = index.get(doc_source+doc_id);
+				logger.debug(val);
+				logger.debug(val.words);
+				if (val.words.containsKey(term))
 				{
-//					logger.debug("Hit: ");
-					Document d = searcher.doc(hit.doc);
-//					logger.debug(d.get("title") + " " + d.get("id"));
-					CMSDocumentDao dao = getDbo(d.get("source"));
-					
-					ret.add(dao.getDocument(d.get("id")));
+					ret += val.words.get(term);
+					logger.debug("Got a match: " + term + " with a score of: " + val.words.get(term));
 				}
 			}
 		}
 		catch(Exception e)
 		{
-			//logger.error("Problem searching: " + e);
+			logger.error("Problem searching " + doc_source + doc_id + ":" + e);
+			return 0;
 		}
 		
 		
 		return ret;
-	}
-	private void addDoc(CMSDocument cmsdoc) throws IOException
-	{
-		Document doc = new Document();
-		doc.add(new TextField("title", cmsdoc.getTitle(), Field.Store.YES));
-
-		doc.add(new TextField("content", cmsdoc.getContent(), Field.Store.NO));
-		doc.add(new StringField("id",cmsdoc.getId(), Field.Store.YES));
-		doc.add(new StringField("source",cmsdoc.getSource(), Field.Store.YES));
-		writer.addDocument(doc);
 	}
 
 	public CMSDocumentDao getDbo(String name)
@@ -157,5 +109,60 @@ public class CMSSearchIndex
 			if (ds.getDaoName().equals(name))
 				dbo = ds;
 		return dbo;
+	}
+	
+	private class IndexEntry
+	{
+		private String source;
+		private String id;
+		private String title;
+		public Map<String,Integer> words;
+		
+		public IndexEntry(String source, String id, String title, String content, List<String> keywords)
+		{
+			this.source = source;
+			this.id = id;
+			this.title = title;
+			
+			//Remove html and non alpha numeric characters
+			content = Jsoup.parse(content).text();
+			content = content.replaceAll("[^A-Za-z0-9 ]", "");
+			
+			words = new HashMap<>();
+			for(String word:content.split(" "))
+				if(words.containsKey(word))
+					words.put(word,words.get(word)+1);
+				else
+					words.put(word,1);
+
+			for(String word:keywords)
+				if(words.containsKey(word))
+					words.put(word,words.get(word)+100);
+				else
+					words.put(word,100);
+		}
+		public IndexEntry(CMSDocument doc)
+		{
+			this.source = doc.getSource();
+			this.id = doc.getId();
+			this.title = doc.getTitle();
+			
+			//Remove html and non alpha numeric characters
+			String content = Jsoup.parse(doc.getContent()).text();
+			content = content.replaceAll("[^A-Za-z0-9 ]", "");
+			
+			words = new HashMap<>();
+			for(String word:content.split(" "))
+				if(words.containsKey(word))
+					words.put(word,words.get(word)+1);
+				else
+					words.put(word,1);
+
+			for(String word:doc.getKeyTerms().split(" "))
+				if(words.containsKey(word))
+					words.put(word,words.get(word)+100);
+				else
+					words.put(word,100);
+		}
 	}
 }
