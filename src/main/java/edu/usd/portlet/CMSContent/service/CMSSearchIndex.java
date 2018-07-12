79793ba1a3ba9e10d7cf8aa55a7fdd8b355bb9aa
Toben Archer <Toben.Archer@usd.edu>
2018-07-12 08:50:00 -0500

    Uncommented search.
    I had commented out search in one of my branches to speed up the
    deployment while I was developing the portlet. That snuck into my
    main branch and broke search. Uncommented it now so it shouldn't
    cause any more problems.

--- src/main/java/edu/usd/portlet/CMSContent/service/CMSSearchIndex.java
+++ src/main/java/edu/usd/portlet/CMSContent/service/CMSSearchIndex.java
@@ -57,26 +57,26 @@ public class CMSSearchIndex
 		{
 
 //Temporarily removed to save development time.
-//			for(CMSDocumentDao dao: dataSources)
-//			{
-//				logger.info("Indexing " + dao.getDaoName());
-//				List<CMSDocument> docs = dao.getAllDocumentsContentless();
-//				for(CMSDocument doc: docs)
-//				{
-//					try
-//					{
-//						CMSDocument val = dao.getDocument(doc.getId());
-//						IndexEntry ie = new IndexEntry(val);
-//						index.put(dao.getDaoName() + doc.getId(),ie);
-//						doc_index.put(dao.getDaoName() + doc.getId(),val.render());
-//					}
-//					catch(IllegalArgumentException e)
-//					{
-//						logger.error("Error indexing document: " + doc.getId() + " Error: " + e);
-//					}
-//				}
-//			}
-//			logger.info("" + index.size() + " documents were indexed");
+			for(CMSDocumentDao dao: dataSources)
+			{
+				logger.info("Indexing " + dao.getDaoName());
+				List<CMSDocument> docs = dao.getAllDocumentsContentless();
+				for(CMSDocument doc: docs)
+				{
+					try
+					{
+						CMSDocument val = dao.getDocument(doc.getId());
+						IndexEntry ie = new IndexEntry(val);
+						index.put(dao.getDaoName() + doc.getId(),ie);
+						doc_index.put(dao.getDaoName() + doc.getId(),val.render());
+					}
+					catch(IllegalArgumentException e)
+					{
+						logger.error("Error indexing document: " + doc.getId() + " Error: " + e);
+					}
+				}
+			}
+			logger.info("" + index.size() + " documents were indexed");
 			logger.info("Indexer built");
 		}
 		catch(Exception e)
